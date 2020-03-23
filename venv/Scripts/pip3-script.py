f5104811ad3425732b201b552b2ea7c6e8ede5d6
Ashutosh Saxena <ashutoshsaxena788@gmail.com>
2020-03-23 07:21:16 +0530

    New message...

--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!"C:\Python Programs\venv\Scripts\python.exe"
+#!"C:\Python Projects\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
