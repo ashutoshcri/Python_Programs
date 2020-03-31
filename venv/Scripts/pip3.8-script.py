02ad30519228b8f20a15fcb095e41b7217ffffbf
Ashutosh Saxena <ashutoshsaxena788@gmail.com>
2020-03-31 13:14:23 +0530

    March 31st 2020

--- venv/Scripts/pip3.8-script.py
+++ venv/Scripts/pip3.8-script.py
@@ -1,4 +1,4 @@
-#!"C:\Python Projects\venv\Scripts\python.exe"
+#!C:\Python_Programs\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3.8'
 __requires__ = 'pip==19.0.3'
 import re
